2b770a6b1b694510ac3db21080fb6c347212bd3a
Yuriy Golota <ypgolota@pm.me>
2022-12-15 21:33:23 -0500

    wip: fails with edge cases

--- leetcode/410_split_array_largest_sum.py
+++ leetcode/410_split_array_largest_sum.py
@@ -2,12 +2,14 @@
 
 # nums = [2, 53, 12334, 3423, 535]
 # k = 3
+# print(sum(nums))
+# print(sum(nums)/k)
 
-# nums = [10,10,11,1,1]
-# k = 3
+nums = [10,10,11,1,1]
+k = 3
 
 nums = [7,2,5,14,8]
-k = 3
+k = 2
 
 def splitArray(nums: list[int], k: int) -> int:
     # Initialize pointers
@@ -18,16 +20,16 @@ def splitArray(nums: list[int], k: int) -> int:
     solution = []
     # Calculate ideal subarray sum (i.e. smallest possible answer)
     ideal_subarray_sum = sum(nums) / k
-    
-    while l <= r:
+
+    while l < r:
         L.append(nums[l])
         R.append(nums[r])
     
-        while sum(L) < ideal_subarray_sum and l + 1 < r:
+        while sum(L) <= ideal_subarray_sum:
             l += 1
             L.append(nums[l])
         
-        while sum(R) < ideal_subarray_sum and l < r - 1:
+        while sum(R) <= ideal_subarray_sum:
             r -= 1
             R.append(nums[r])
         
@@ -37,18 +39,16 @@ def splitArray(nums: list[int], k: int) -> int:
             # Move pointers
             l += 1
             r = r + (len(R) - 1)
-            # Clear builder lists
-            L.clear()
-            R.clear()
         elif sum(L) > sum(R) and sum(R) > ideal_subarray_sum:
             # Append to solution
             solution.append(sum(R))
             # Move pointers
             l = l - (len(L) - 1)
             r -= 1
-            # Clear builder lists
-            L.clear()
-            R.clear()
+        
+        # Clear builder lists
+        L.clear()
+        R.clear()
     
     solution.sort()
     return solution[-1]
